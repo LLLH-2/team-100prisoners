7302d399fd39eea558209d9f872877e7cf00c70c
tmttd <pqstvx929@gmail.com>
2024-04-22 18:00:27 +0900

    feat: Make Prisoner select number and return the answer
    Use for and if break to create function.

--- main.py
+++ main.py
@@ -7,3 +7,39 @@ def generate_prisoner_num():
 
 prisoner_num_list = generate_prisoner_num()
 print(prisoner_num_list)
+
+prisoner_pick = [random.randint(1, 100) for _ in range(50)]
+
+print(prisoner_pick)
+
+# 죄수 및 정답 순서
+
+order = 0
+answer = prisoner_num_list[order]
+print(answer)
+
+for try_num in range(50+1):
+    selection = prisoner_pick[try_num]
+    print(selection)
+
+    if selection == answer:
+        print('정답을 맞혔습니다. 다음 죄수로 넘어갑니다.')
+        order += 1
+        break
+
+
+
+
+
+
+    
+
+
+# for order in range(0, 99):
+
+#     def choice_by_prisoner(i):
+        
+#         answer = prisoner_num_list[order]
+
+#         if choose == answer:
+#             order += 1
